c951bfb645b57c01dd02a389b1fae083bf6d2f38
cemosan61 <165674593+cemosan61@users.noreply.github.com>
2025-03-16 00:04:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    document.getElementById('nav-menu').classList.toggle('active');
+}
+
+// Array der Begrüßungstexte
+const headings = ["Willkommen", "Rojbas", "Hos geldiniz"];
+
+// Index, um durch die Texte zu iterieren
+let currentIndex = 0;
+
+// Referenz auf unser h1-Element
+const headingElement = document.getElementById("rotating-text");
+
+// Funktion zum Ändern des angezeigten Textes
+function rotateHeading() {
+    headingElement.textContent = headings[currentIndex];
+    currentIndex = (currentIndex + 1) % headings.length;
+}
+
+// Wechsle alle 2 Sekunden den Text
+setInterval(rotateHeading, 2000);
+
